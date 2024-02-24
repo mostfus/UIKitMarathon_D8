f064626573968513947eef8ff5b2836f78e39aa6
mostfus <mostfus@yandex-team.ru>
2024-02-24 10:54:30 +0300

    Initial Commit

---
+++ UIKitMarathon_D8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitMarathon_D8
+//
+//  Created by Maksim Vaselkov on 24.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
